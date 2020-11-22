4bbea629b22f79042e0895482341bf0000ff2016
Mark Wainwright <wainwrightml@gmail.com>
2020-11-22 14:38:41 +0000

    Improved loading screen

--- Puns.Blazor/Pages/PunState.cs
+++ Puns.Blazor/Pages/PunState.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Pronunciation;
 using WordNet;
 
@@ -14,6 +15,20 @@ namespace Puns.Blazor.Pages
             WordNetEngine = new Lazy<WordNetEngine>(()=> new WordNetEngine());
 
             PronunciationEngine = new Lazy<PronunciationEngine>(()=> new PronunciationEngine());
+
+            var tasks = new List<Task>()
+            {
+                Task.Factory.StartNew(()=>WordNetEngine.Value),
+                Task.Factory.StartNew(() => PronunciationEngine.Value)
+            };
+
+            Task.WhenAll(tasks).ContinueWith(x =>
+            {
+                EnginesLoaded = true;
+                PageLoaded(null, EventArgs.Empty);
+            });
+
+
         }
 
 
@@ -26,12 +41,15 @@ namespace Puns.Blazor.Pages
 
         public HashSet<SynSet> CrossedOffSynsets { get; } = new HashSet<SynSet>();
 
+        public event EventHandler PageLoaded;
 
         public IReadOnlyCollection<IGrouping<string, Pun>>? PunList { get; set; }
 
         public HashSet<string> RevealedWords { get; } = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
 
         public bool IsGenerating { get; set; } = false;
+
+        public bool EnginesLoaded { get; private set; } = false;
 
         public Lazy<WordNetEngine> WordNetEngine { get; }
 
